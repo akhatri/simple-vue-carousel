63792594907fda27e21dd6c44defce1cce9a5954
akhatri <akshatster@gmail.com>
2019-08-26 12:30:00 +1000

    responsive mode

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -15,9 +15,20 @@
     </div>
 
     <div class="carousel-controls">
-      <button aria-label="Previous" class="btn" @click="previous()">Previous</button>
-      <button aria-label="Next" class="btn" @click="next()">Next</button>
+      <button aria-label="Previous" class="btn previous" @click="previous()">Previous</button>
+      <button aria-label="Next" class="btn next" @click="next()">Next</button>
     </div>
+
+    <div class="mobile-controls">
+      <button aria-label="Previous" class="btn previous" @click="previous()">
+        <img alt="previous arrow" src="images/arrow.svg"/>
+      </button>
+      <button aria-label="Next" class="btn next" @click="next()">
+        <img alt="next arrow" src="images/arrow.svg"/>
+      </button>
+    </div>
+
+
   </section>
 </template>
 
@@ -57,11 +68,11 @@ export default class Carousel extends Vue {
   // Click events
   previous() {
     console.log("clicked previous");
-    
-    // remove the last item at the end of array 
+
+    // remove the last item at the end of array
     const lastItemRemoved = this.slidesData.pop();
-    
-    // add the last removed item back to the start of the array 
+
+    // add the last removed item back to the start of the array
 		this.slidesData.unshift(lastItemRemoved);
   }
 
@@ -70,9 +81,9 @@ export default class Carousel extends Vue {
 
     // remove the first item at the beginning of the array
     let firstItemRemoved = this.slidesData.shift();
-    
+
     // add the first removed back to the end of the array
-    this.slidesData.push(firstItemRemoved);	
+    this.slidesData.push(firstItemRemoved);
   }
 
 }
@@ -80,7 +91,9 @@ export default class Carousel extends Vue {
 
 
 <style lang="scss" scoped>
+
 .carousel-container {
+  position: relative;
   margin-top: 20px;
   margin-bottom: 50px;
   width: 100%;
@@ -130,4 +143,69 @@ export default class Carousel extends Vue {
     }
   }
 }
+
+.mobile-controls {
+  display: none;
+}
+
+// Responsive view - mobile
+@media only screen and (max-width: 767px) {
+
+  // show only 1 image in mobile view
+  .carousel-media {
+    padding: 0;
+    .slides {
+      width: 100%;
+      // hide the non-active slides as we have only 1 active slide per view on mobile
+      .slide:not(.is-active) {
+        display: none;
+      }
+      .slide.is-active {
+        flex-basis: 100%;
+      }
+
+    }
+  }
+
+  .carousel-controls {
+    display: none;
+  }
+
+  .mobile-controls {
+    display: block;
+    .btn {
+      background-color: #fe414d;
+      border: 0;
+      padding: 0px;
+      cursor: pointer;
+      img {
+        transform: scale(0.5) translateX(10px);
+        transform-origin: 50% 50%;
+      }
+      &.previous {
+        position: absolute;
+        top: 50%;
+        left: 0px;
+        transform: translateY(-50%) rotate(180deg);
+        border-top-left-radius: 100px;
+        border-bottom-left-radius: 100px;
+        img {
+          //transform: translateX(2px);
+        }
+      }
+      &.next {
+        position: absolute;
+        top: 50%;
+        right: 0px;
+        transform: translateY(-50%);
+        border-top-left-radius: 100px;
+        border-bottom-left-radius: 100px;
+        img {
+          //transform: translateX(0px);
+        }
+      }
+    }
+  }
+}
+
 </style>
